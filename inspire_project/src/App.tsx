9e97e588790656ed68e7d18ed0d0f8efb43a5a05
eunhye <dmsgp2627@naver.com>
2025-12-05 14:29:28 +0900

    darkmode 적용 완료

--- inspire_project/src/App.tsx
+++ inspire_project/src/App.tsx
@@ -1,14 +1,14 @@
 import { AppFooter, AppHeader } from "./components/common";
-
+import { ThemeProvider } from "./components/theme-providar";
 const App = () => {
   return (
-    <div className="page">
-
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="page">
         <AppHeader />
-              <div className="container"></div>
+        <div className="container"></div>
         <AppFooter />
-
-    </div>
+      </div>
+    </ThemeProvider>
   );
 };
 
--- inspire_project/src/components/common/AppHeader.tsx
+++ inspire_project/src/components/common/AppHeader.tsx
@@ -1,23 +1,15 @@
 import { Separator } from "../ui";
 
-
 function AppHeader() {
   return (
     <header className="fixed top-0 z-10 w-full flex items-center justify-center bg-[#121212]">
       <div className="w-full max-w-[1328px] flex items-center justify-between px-6 py-3">
         {/* 로고 & 네비 UI */}
-        <div className="flex items-center gap-5">
-          <div>
-            <img>
-            </img>
-            <div className="font-semibold">
-              토픽 인사이트
-            </div>
-            <Separator orientation="vertical" /> 
-            <div className="font-semibold">
-              포트폴리오
-            </div>
-          </div>
+        <div className="flex items-center gap-6">
+          <img src="/logo.svg" alt="@LOGO" className="w-15 cursor-pointer" />
+          <div className="font-semibold">토픽 인사이트</div>
+          <Separator orientation="vertical" />
+          <div className="font-semibold">포트폴리오</div>
         </div>
 
         {/* 로그인 UI */}
