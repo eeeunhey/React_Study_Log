d1129a385b05cf48971ad457c03079cc0ca7a6e8
eunhye <dmsgp2627@naver.com>
2025-12-06 16:10:54 +0900

    sidebar 컴포넌트로 따로 분리

--- inspire_project/src/App.tsx
+++ inspire_project/src/App.tsx
@@ -1,43 +1,6 @@
-import {
-  ChartNoAxesCombined,
-  ChevronDown,
-  CodeXml,
-  DraftingCompass,
-  Footprints,
-  Goal,
-  Lightbulb,
-  List,
-  Rocket,
-} from "lucide-react";
-import { AppFooter, AppHeader } from "./components/common";
-import { ThemeProvider } from "./components/theme-providar";
-import { Button } from "./components/ui";
 
-const CLASS_CATEGORY = [
-  { id: 1, label: "전체", category: "", icon: <List /> },
-  { id: 2, label: "인문학", category: "humanity", icon: <Lightbulb /> },
-  { id: 3, label: "스타트업", category: "start-up", icon: <Rocket /> },
-  { id: 4, label: "IT 프로그램", category: "programing", icon: <CodeXml /> },
-  { id: 5, label: "서비스 전략 기획", category: "planning", icon: <Goal /> },
-  {
-    id: 6,
-    label: "마케팅",
-    category: "marketing",
-    icon: <ChartNoAxesCombined />,
-  },
-  {
-    id: 7,
-    label: "디자인 일러스트",
-    category: "design",
-    icon: <DraftingCompass />,
-  },
-  {
-    id: 8,
-    label: "자기계발",
-    category: "self-development",
-    icon: <Footprints />,
-  },
-];
+import { AppFooter, AppHeader, AppSidebar } from "./components/common";
+import { ThemeProvider } from "./components/theme-providar";
 
 const App = () => {
   return (
@@ -45,33 +8,9 @@ const App = () => {
       <div className="page">
         <AppHeader />
         <div className="container">
-          {/* 카테고리 사이드바 */}
-          <main className="w-full h-full min-h-[720px] flex p-6 gap-6">
-            <aside className="w-60 flex flex-col gap-6">
-              <div className="flex items-center gap-2">
-                {/* Shadcn UI Typograpy h4 제공 컴포넌트 사용 */}
-                <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-                  카테고리
-                </h4>
-                <ChevronDown className="mt-1" />
-              </div>
 
-              <div className="w-full flex flex-col gap-2 ">
-                {CLASS_CATEGORY.map((menu) => {
-                  return (
-                    <Button
-                      key={menu.id}
-                      variant={"ghost"}
-                      className="justify-start text-muted-foreground hover:text-white hover:pl-6 transition-all duration-500"
-                    >
-                      {}
-                      {menu.icon}
-                      {menu.label}
-                    </Button>
-                  );
-                })}
-              </div>
-            </aside>
+          <main className="w-full h-full min-h-[720px] flex p-6 gap-6">
+            <AppSidebar />
             {/* 토픽 컨텐츠 */}
             <section></section>
           </main>
